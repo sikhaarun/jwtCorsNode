f1aea560df682124c304fac1d71da71cb0ccd488
Sikha Arun <sikha@oxyten.com>
2019-04-29 13:16:15 +0530

    second commit

--- app.js
+++ app.js
@@ -51,8 +51,12 @@ var corsOptions = {
 //   }
 // }
 
-app.use(cors(corsOptions))
-
+//app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://example.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.get('/products/:id',cors(corsOptions), function (req, res, next) {
 //     res.json({msg: 'This is CORS-enabled for all origins!'})
 //   })
