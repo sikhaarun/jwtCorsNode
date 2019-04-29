be99e4c70d18796a91f4fc218fad7931d7c89698
Sikha Arun <sikha@oxyten.com>
2019-04-29 14:08:50 +0530

    first commit

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function(req, res, next) {
   console.log(allowedOrigins.indexOf(origin))
   if(allowedOrigins.indexOf(origin) > -1){
        res.setHeader('Access-Control-Allow-Origin', origin);
-       res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+       res.header('Access-Control-Allow-Methods', 'GET,POST, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.header('Access-Control-Allow-Credentials', true);
         return next();
